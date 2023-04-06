660083fea5238915b5b87c2f3f59403faf50347d
Alina <AliKochneva@yandex.ru>
2023-04-06 23:24:22 +0700

    запрет перехода на страницу подписки при пустом поле email (js)

--- x.js
+++ x.js
@@ -1,6 +1,5 @@
 const footerButton = document.querySelector('.footerSend')
 let footerInput = document.querySelector('.infoinput')
-let SubscribedMail = document.querySelector('.subscribedContent')
 
 footerButton.addEventListener('click', function(){
     if (footerInput.value != ''){
